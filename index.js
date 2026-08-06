75f5e4cb4fee76d50516dc25d082fd8fa53321aa
saifitgenics <saifitgenics@gmail.com>
2026-08-06 11:18:56 +0500

    page updated

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 import express from 'express';
-
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.get('/', (req, res) => {
-  res.send('its me ');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, "0.0.0.0",() => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
